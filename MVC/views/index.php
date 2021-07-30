e817acf1826e08f3ad4da91ec4bbfa1fdd614249
hangntt1210 <nguyenthuyhang991012@gmail.com>
2021-07-30 10:13:54 +0700

    fix fontend

--- MVC/views/index.php
+++ MVC/views/index.php
@@ -163,13 +163,15 @@
                        
                         </tbody>
                     </table>
+                    <div class="d-flex flex-row">
                     <?php 
                         $total_page = $data["sotrang"];
                         for($t=1; $t<=$total_page; $t++){
-                            echo "<button><a href='./User/Show/$t'>$t </a></button>";
+                            echo "<button><a href='./User/Show/$t'>$t </a></button> ";
                         }
                         
                     ?>
+                    </div>
                 </div>
                 <!-- /.row -->
          
